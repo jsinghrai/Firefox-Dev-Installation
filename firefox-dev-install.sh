6826f0e5a74a7b090700461b72d4ad318469aeb8
Jaspal Singh <jsingh49@ucmerced.edu>
2019-02-19 19:31:38 -0800

    Gives user the choice to begin from the different step in case the script failed previous time

--- firefox-dev-install.sh
+++ firefox-dev-install.sh
@@ -3,6 +3,15 @@
 # Go to Downloads folder where the file will be downloaded
 cd ~/Downloads
 
+printf "There are four steps in installation process: \n"
+printf "1. Dowloading the file from Mozilla. \n"
+printf "2. Decompressing the file. \n"
+printf "3. Move the Application to /usr/local \n"
+printf "4. Creating the Desktop file for browser. \n"
+
+read -p 'Please choose which step to begin from (default 1): ' step
+step=${step:-1}
+if (( step == 1 )); then 
 # Download the file from Mozilla's website via wget
 printf "Downloading the file from Mozilla. \n"
 
@@ -13,8 +22,11 @@ if [[ $? -ne 0 ]]; then
  	exit 1
 else
 	printf "File has been downloaded successfully. \n"
+	let "step+=1"
+fi
 fi
 
+if (( step == 2 )); then
 # Decompress the file
 printf "Decompressing the file. \n"
 
@@ -25,8 +37,11 @@ if [[ $? -ne 0 ]]; then
  	exit 1
 else
 	printf "File has been decompress successfully. \n"
+	let "step+=1"
+fi
 fi
 
+if (( step == 3 )); then
 # Move the Application to /usr/local
 printf "Moving the Browser folder to /usr/local/. \n"
 
@@ -37,8 +52,11 @@ if [[ $? -ne 0 ]]; then
  	exit 1
 else
 	printf "The folder was moved successfully. \n"
+	let "step+=1"
+fi
 fi
 
+if (( step == 4 )); then
 # Create the desktop file to run it without terminal
 printf "Creating the desktop file for Browser. \n"
 
@@ -49,4 +67,5 @@ if [[ $? -ne 0 ]]; then
  	exit 1
 else
 	printf "File has been created successfully. \n"
+fi
 fi
